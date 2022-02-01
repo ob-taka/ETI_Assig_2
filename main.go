fb1efa8eb3d2b470a5bd965be50c62a1cf48b742
Runlin Xiong <s10189595@connect.np.edu.sg>
2022-02-01 12:07:14 +0800

    API response added

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"github.com/gorilla/mux"
 )
 
+//Final module json object to be pick up by frontend
 type Module struct {
 	Mod_ID    string   `json:"mod_id"`
 	Mod_Name  string   `json:"mod_name"`
@@ -22,7 +23,55 @@ type Module struct {
 	Mod_RnC   string   `json:"mod_rnc"`
 }
 
-//test
+//API Json response from Module management microservices
+type ModAPI struct {
+	Data struct {
+		Modules []struct {
+			LearningObj string `json:"learningObj"`
+			ModuleCode  string `json:"moduleCode"`
+			ModuleName  string `json:"moduleName"`
+			Synopsis    string `json:"synopsis"`
+		} `json:"modules"`
+	} `json:"data"`
+}
+
+type SearchModAPI struct {
+	Data struct {
+		SearchModules []struct {
+			LearningObj string `json:"learningObj"`
+			ModuleCode  string `json:"moduleCode"`
+			ModuleName  string `json:"moduleName"`
+			Synopsis    string `json:"synopsis"`
+		} `json:"searchModules"`
+	} `json:"data"`
+}
+
+//API Json response form class management microservices
+type classAPI struct {
+	SemesterModules []struct {
+		ModuleClasses []struct {
+			Capacity  int64    `json:"Capacity"`
+			ClassCode string   `json:"ClassCode"`
+			Schedule  string   `json:"Schedule"`
+			Students  []string `json:"Students"`
+			Tutor     string   `json:"Tutor"`
+		} `json:"ModuleClasses"`
+		ModuleCode string `json:"ModuleCode"`
+		ModuleName string `json:"ModuleName"`
+	} `json:"SemesterModules"`
+	SemesterStartDate string `json:"SemesterStartDate"`
+}
+
+//Custome module class structure to be parse from API response
+type modClass struct {
+	moduleCode string   `json:"ModuleCode"`
+	student    []string `json:"Student"`
+	tutor      []string `json:"Tutor"`
+	classCode  []string `json:"ClassCode"`
+}
+
+//Connect to build in database
+//incase API response dont work
 func connect() *sql.DB {
 	db, err := sql.Open("mysql", "test:password@tcp(db:3306)/Modules")
 
@@ -34,6 +83,7 @@ func connect() *sql.DB {
 	return db
 }
 
+//Get all module from the database and map to Module struct
 func getAllMod() []Module {
 
 	db := connect()
@@ -61,6 +111,119 @@ func getAllMod() []Module {
 	return moudles
 }
 
+//Get all module available from Moudle managements API
+func getModAPI() []Module {
+	var module []Module
+	var temp Module
+	var mod ModAPI
+
+	// //as of 31 Jan Module API only provide module name and synopsis
+	// url := "http://localhost:8114/query"
+	// // required JSON body as per design of the API
+	// jsonBody := map[string]string{"query": "query ListModules(){modules(){name, synopsis}}"}
+	// jsonValue, _ := json.Marshal(jsonBody)
+	// request, _ := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(jsonValue))
+	// request.Header.Set("Content-Type", "application/json")
+	// client := &http.Client{}
+	// response, err := client.Do(request)
+	// if err != nil{
+	// 	fmt.Println("request failed")
+	// 	return module
+	// }
+	// resBody, _ := ioutil.ReadAll(response.Body)
+	// jsonArr := json.Unmarshal(resBody, &mod)
+
+	//assuming the API call works, the body of the API response structure should be as follows:
+	data := `{"data":{
+		"modules":[
+			{"moduleCode": "CM", 
+			"moduleName": "Computing Math", 
+			"synopsis": "Learn about computing math",
+			 "learningObj": "learn CM"
+			 },
+			 {"moduleCode": "DB",
+			  "moduleName": "Data Base", 
+			  "synopsis": "Learn about Data Base", 
+			  "learningObj": "learn DB"
+			  }
+			]
+		}
+	}`
+	json.Unmarshal([]byte(data), &mod)
+	class := getClassAPI()
+	for _, i := range mod.Data.Modules {
+		temp.Mod_ID = i.ModuleCode
+		temp.Mod_Name = i.ModuleName
+		temp.Mod_Syno = i.Synopsis
+		temp.Mod_LO = i.LearningObj
+		temp.Mod_RnC = "Moudle is fun" // ask 3.9 idiot...
+		for _, c := range class {
+			if temp.Mod_ID == c.moduleCode {
+				temp.Mod_Class = append(temp.Mod_Class, c.classCode...)
+				temp.Student = append(temp.Student, c.student...)
+				temp.Tutor = append(temp.Tutor, c.tutor...)
+			}
+		}
+		module = append(module, temp)
+		temp = Module{}
+	}
+	return module
+}
+
+//Get sepcified from Moudle managements API
+func searchModAPI(modCode string) []Module {
+	var module []Module
+	var temp Module
+	var mod SearchModAPI
+
+	// //as of 31 Jan Module API only provide module name and synopsis
+	// url := "http://localhost:8114/query"
+	// // required JSON body as per design of the API
+	// jsonBody := map[string]string{"query": "query SearchModules($text: String!){searchModules(text: $text){name, synopsis}}, "variables":{"text": modCode}"}
+	// jsonValue, _ := json.Marshal(jsonBody)
+	// request, _ := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(jsonValue))
+	// request.Header.Set("Content-Type", "application/json")
+	// client := &http.Client{}
+	// response, err := client.Do(reque st)
+	// if err != nil{
+	// 	fmt.Println("request failed")
+	// 	return module
+	// }
+	// resBody, _ := ioutil.ReadAll(response.Body)
+	// jsonArr := json.Unmarshal(resBody, &mod)
+
+	//assuming the API call works, the body of the API response structure should be as follows:
+	data := `{"data":{
+		"searchModules":[
+			{"moduleCode": "CM", 
+			"moduleName": "Computing Math", 
+			"synopsis": "Learn about computing math",
+			 "learningObj": "learn CM"
+			 }
+			]
+		}
+	}`
+	json.Unmarshal([]byte(data), &mod)
+	class := getClassAPI()
+	for _, i := range mod.Data.SearchModules {
+		temp.Mod_ID = i.ModuleCode
+		temp.Mod_Name = i.ModuleName
+		temp.Mod_Syno = i.Synopsis
+		temp.Mod_RnC = "Moudle is fun" // ask 3.9 idiot...
+		for _, c := range class {
+			if temp.Mod_ID == c.moduleCode {
+				temp.Mod_Class = append(temp.Mod_Class, c.classCode...)
+				temp.Student = append(temp.Student, c.student...)
+				temp.Tutor = append(temp.Tutor, c.tutor...)
+			}
+		}
+		module = append(module, temp)
+		temp = Module{}
+	}
+	return module
+}
+
+//Get specified module from database: Search
 func getMod(id string) []Module {
 
 	db := connect()
@@ -88,6 +251,7 @@ func getMod(id string) []Module {
 	return moudles
 }
 
+//Get tutors ID assigned base the module code arguement parse in
 func getTutor(id string) []string {
 	db := connect()
 	results, err := db.Query("select tID from tutor where tMoulde = ?", id)
@@ -109,6 +273,7 @@ func getTutor(id string) []string {
 	return tutor
 }
 
+//Get students ID assigned base on the module code arguement parse in
 func getStudent(id string) []string {
 	db := connect()
 	results, err := db.Query("SELECT stuID FROM student WHERE sModuel = ?", id)
@@ -129,6 +294,7 @@ func getStudent(id string) []string {
 	return student
 }
 
+//Get class code assigned base on the module code argument parse in
 func getClass(id string) []string {
 	db := connect()
 
@@ -150,10 +316,66 @@ func getClass(id string) []string {
 	return class
 }
 
-//user page test
-func showAllMod(w http.ResponseWriter, r *http.Request) {
+//Get all the classes
+func getClassAPI() []modClass {
+	/*
+	   Class management API it is unable to retrive any infromation without a semester start date
+	   hence a date would be hardcoded, idealy the API should response with all current actived
+	   modules unless specified
+	*/
+	var result []modClass
+	var class classAPI
+	var temp modClass
 
-	mod := getAllMod()
+	// startDate :=  "16-01-2022"
+	// url := "localhost:8041/api/v1/classes/" + startDate
+	// response, err := http.Get(url)
+	// if err != nil {
+	// 	fmt.Println("request failed")
+	// 	return result
+	// }
+	// data, _ := ioutil.ReadAll(response.Body)
+	// jsonArr := json.Unmarshal([]byte(data), &class)
+
+	//Assuming the API successfully respond the response body should be as follows:
+	data := `{"SemesterModules":[
+		{"ModuleClasses":[
+			{"Capacity":10,
+			"ClassCode":"ADB02",
+			"Schedule":"17-01-2022 10:00:00",
+			"Students": [],
+			"Tutor":"T001"
+			},
+			{"Capacity":11,
+			"ClassCode":"ADB01",
+			"Schedule":"17-01-2022 10:00:00",
+			"Students":["S004","S005","S006"],
+			"Tutor":"T002"
+			}],
+			"ModuleCode":"CM",
+			"ModuleName":"Computing Math"}],
+			"SemesterStartDate":"16-01-2022"}`
+	json.Unmarshal([]byte(data), &class)
+
+	for _, i := range class.SemesterModules {
+		temp.moduleCode = i.ModuleCode
+		for _, x := range i.ModuleClasses {
+			temp.classCode = append(temp.classCode, x.ClassCode)
+			temp.student = append(temp.student, x.Students...)
+			temp.tutor = append(temp.tutor, x.Tutor)
+		}
+		result = append(result, temp)
+	}
+
+	return result
+}
+
+func showAllMod(w http.ResponseWriter, r *http.Request) {
+	//Retrive data from Database
+	//mod := getAllMod()
+
+	//Retrive data from API response
+	mod := getModAPI()
 
 	json.NewEncoder(w).Encode(mod)
 }
@@ -161,7 +383,9 @@ func showAllMod(w http.ResponseWriter, r *http.Request) {
 func searchMod(w http.ResponseWriter, r *http.Request) {
 
 	params := mux.Vars(r)
-	mod := getMod(params["id"])
+
+	//mod := getMod(params["id"])
+	mod := searchModAPI(params["id"])
 
 	json.NewEncoder(w).Encode(mod)
 }
